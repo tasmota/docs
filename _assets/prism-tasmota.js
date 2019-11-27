26702464c20bf4dfac49ad8d60bf9f15c27db30c
blakadder <blakadder@users.noreply.github.com>
2019-11-27 11:28:08 +0100

    Update prism-tasmota.js

--- _assets/prism-tasmota.js
+++ _assets/prism-tasmota.js
@@ -1,6 +1,6 @@
 Prism.languages.console = {
 	'operator': /^\b(?:rule)\d?\b/im,
-	'number': /\b(?<=on)\s\S{1,}#\S{1,}\s/i,
+	'number': /\b(?<=on)\s\S{1,}#\S{1,}\b/i,
 	'selector': /(?<!(power\w+))\s(?:on|do|endon|break|if|else|elseif|and|or)\b/i,
 	'regex': /\b(?:backlog)\b/i,
 	'comment': /[<]\w+[>]/,
