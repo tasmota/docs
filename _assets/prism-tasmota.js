92ed5127affa42e7eb923623e840d7e56352ce77
blakadder <blakadder@users.noreply.github.com>
2019-11-27 11:20:12 +0100

    Update prism-tasmota.js

--- _assets/prism-tasmota.js
+++ _assets/prism-tasmota.js
@@ -4,7 +4,7 @@ Prism.languages.console = {
 	'selector': /(?<!(power\w+))\s(?:on|do|endon|break|if|else|elseif|and|or)\b/i,
 	'regex': /\b(?:backlog)\b/i,
 	'comment': /[<]\w+[>]/,
-	'important': /^\w+/im,
+	'important': /(?<!\ )^\w+im,
 	'deleted': /[;]\s/,
 	'json': {
 		pattern: /[{].*[}]/,
